d17baff69e1f9ffe3787f82a4821d31d947391ec
Anzood <tolstoy.andrey@gmail.com>
2024-11-18 13:23:11 +0200

    Fixed tabs

--- lua/kickstart/plugins/bufferline.lua
+++ lua/kickstart/plugins/bufferline.lua
@@ -1,10 +1,27 @@
--- In lua/kickstart/plugins/bufferline.lua
 return {
   'akinsho/bufferline.nvim',
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   event = 'VimEnter',
   init = function()
+    -- Function to find and close [No Name] buffer
+    local function close_no_name_buffer()
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        -- Check if buffer is listed and has no name
+        if vim.fn.buflisted(buf) == 1 and vim.api.nvim_buf_get_name(buf) == '' then
+          vim.cmd('bd ' .. buf)
+          break
+        end
+      end
+    end
+
+    -- Function to focus neo-tree and clean up
+    local function focus_neo_tree_and_cleanup()
+      vim.cmd 'Neotree focus'
+      -- Use vim.schedule to ensure neo-tree gets focus before we close the buffer
+      vim.schedule(close_no_name_buffer)
+    end
+
     -- Define the safe close function in the global namespace
     vim.api.nvim_create_user_command('SafeCloseBuffer', function()
       -- Get the current buffer number
@@ -22,11 +39,24 @@ return {
         -- Then close the original buffer
         vim.cmd('bd ' .. current_buf)
       else
-        -- If it's the last buffer, create a new one before closing this one
-        vim.cmd 'enew'
-        vim.cmd('bd ' .. current_buf)
+        -- If it's the last buffer, recreate the initial state
+        vim.cmd 'enew' -- Create new buffer
+        vim.cmd('bd ' .. current_buf) -- Close the original buffer
+        -- Focus neo-tree and clean up
+        vim.schedule(focus_neo_tree_and_cleanup)
       end
     end, {})
+
+    -- Set up autocmd to handle [No Name] buffer when opening files
+    vim.api.nvim_create_autocmd('BufEnter', {
+      callback = function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        -- If entering a named buffer (actual file)
+        if bufname ~= '' then
+          vim.schedule(close_no_name_buffer)
+        end
+      end,
+    })
   end,
   opts = {
     options = {
@@ -53,44 +83,50 @@ return {
       },
     },
     highlights = {
+      -- Background colors
       background = {
         bg = '#3c3836',
-      },
-      buffer_selected = {
-        bg = '#504945',
-        bold = true,
-        italic = false,
-      },
-      buffer_visible = {
-        bg = '#3c3836',
-      },
-      close_button = {
-        bg = '#3c3836',
-        fg = '#928374',
-      },
-      close_button_selected = {
-        fg = '#ebdbb2',
-        bg = '#504945',
-      },
-      close_button_visible = {
-        fg = '#928374',
       },
       fill = {
         bg = '#3c3836',
       },
-      indicator_selected = {
-        fg = '#fabd2f',
+
+      -- Regular buffer
+      buffer = {
+        bg = '#3c3836',
+        fg = '#928374',
       },
+      buffer_visible = {
+        bg = '#3c3836',
+        fg = '#928374',
+      },
+      buffer_selected = {
+        bg = '#504945',
+        fg = '#ebdbb2',
+        bold = true,
+        italic = false,
+      },
+
+      -- Modified indicator
       modified = {
-        fg = '#fabd2f',
-      },
-      modified_selected = {
+        bg = '#3c3836', -- Match unselected tab bg
         fg = '#fabd2f',
       },
       modified_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
         fg = '#fabd2f',
       },
+      modified_selected = {
+        bg = '#504945', -- Match selected tab bg
+        fg = '#fabd2f',
+      },
+
+      -- Separator
       separator = {
+        bg = '#3c3836',
+        fg = '#3c3836',
+      },
+      separator_visible = {
         bg = '#3c3836',
         fg = '#3c3836',
       },
@@ -98,17 +134,92 @@ return {
         bg = '#504945',
         fg = '#3c3836',
       },
-      separator_visible = {
-        bg = '#3c3836',
-        fg = '#3c3836',
+
+      -- Indicators
+      indicator_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+        fg = '#fabd2f',
       },
-      tab = {
-        bg = '#3c3836',
-        fg = '#928374',
+      indicator_selected = {
+        bg = '#504945', -- Match selected tab bg
+        fg = '#fabd2f',
       },
-      tab_selected = {
-        bg = '#504945',
-        fg = '#ebdbb2',
+
+      -- Diagnostics
+      error = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      error_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      error_selected = {
+        bg = '#504945', -- Match selected tab bg
+      },
+      error_diagnostic = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      error_diagnostic_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      error_diagnostic_selected = {
+        bg = '#504945', -- Match selected tab bg
+      },
+
+      warning = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      warning_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      warning_selected = {
+        bg = '#504945', -- Match selected tab bg
+      },
+      warning_diagnostic = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      warning_diagnostic_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      warning_diagnostic_selected = {
+        bg = '#504945', -- Match selected tab bg
+      },
+
+      info = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      info_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      info_selected = {
+        bg = '#504945', -- Match selected tab bg
+      },
+      info_diagnostic = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      info_diagnostic_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      info_diagnostic_selected = {
+        bg = '#504945', -- Match selected tab bg
+      },
+
+      hint = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      hint_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      hint_selected = {
+        bg = '#504945', -- Match selected tab bg
+      },
+      hint_diagnostic = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      hint_diagnostic_visible = {
+        bg = '#3c3836', -- Match unselected tab bg
+      },
+      hint_diagnostic_selected = {
+        bg = '#504945', -- Match selected tab bg
       },
     },
   },
@@ -123,7 +234,7 @@ return {
     -- Alternative navigation with Shift-h/l
     { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Previous buffer' },
     { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next buffer' },
-    -- Close current buffer (now using our safe close command)
+    -- Close current buffer
     { '<leader>bc', '<cmd>SafeCloseBuffer<cr>', desc = 'Close current buffer' },
     -- Close all buffers except current
     { '<leader>bo', '<cmd>BufferLineCloseOthers<cr>', desc = 'Close other buffers' },
